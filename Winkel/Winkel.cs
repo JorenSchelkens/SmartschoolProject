154afd833d7ed99e21678dad8cae97da2c81ad1f
Dries Leyers <52953642+DriesLeyers@users.noreply.github.com>
2020-01-24 14:52:52 +0100

    Added subdomains
    bank en winkel

---
+++ Winkel/Winkel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Winkel
+{
+    class Winkel
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
