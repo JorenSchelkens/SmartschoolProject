6b45283be50c66b52d5f5d76a7494f1e39b7a8cf
Dries Leyers <52953642+DriesLeyers@users.noreply.github.com>
2020-01-24 14:34:30 +0100

    Subdomain toegevoegd
    Subdomain toegevoegd, klasse lokaal

---
+++ InventarisDomain/Lokaal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventarisDomain
+{
+    public class Lokaal
+    {
+        public string klasVerantwoordelijke { get; set;}
+        public int lokaalNr { get;set;}
+        public int aantalBanken { get; set;}
+        public int aantalBeamers { get; set;}
+        public int aantalStoelen { get; set;}
+        public int aantalComputers { get; set;}
+        public int aantalSchermen { get; set;}
+
+    }
+}
