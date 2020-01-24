7308f6c4b97ab3139ad5538cc3962cde77c5c7bd
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2020-01-24 15:12:22 +0100

    Fixes

--- InventarisDomain/Lokaal.cs
+++ InventarisDomain/Lokaal.cs
@@ -5,14 +5,14 @@ namespace InventarisDomain
 {
     public class Lokaal
     {
-        public string klasVerantwoordelijke { get; set;}
-        public int lokaalNr { get;set;}
-        public int aantalBanken { get; set;}
-        public int aantalBeamers { get; set;}
-        public int aantalStoelen { get; set;}
-        public int aantalComputers { get; set;}
-        public int aantalSchermen { get; set;}
-        
+        public string klasVerantwoordelijke { get; set; }
+        public int lokaalNr { get; set; }
+        public int aantalBanken { get; set; }
+        public int aantalBeamers { get; set; }
+        public int aantalStoelen { get; set; }
+        public int aantalComputers { get; set; }
+        public int aantalSchermen { get; set; }
+
 
     }
 }
