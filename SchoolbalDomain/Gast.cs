d0429a81e712394500bb7357dd9bfd4a1a729877
Dries Leyers <52953642+DriesLeyers@users.noreply.github.com>
2020-03-02 15:58:35 +0100

    Inschrijving formulier af

---
+++ SchoolbalDomain/Gast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolbalDomain
+{
+    public class Gast
+    {
+        public string Naam { get; set; }
+        public bool Confirmed { get; set; }
+
+        public Gast(string naam)
+        {
+            this.Naam = naam;
+            Confirmed = false;
+        }
+    }
+}
