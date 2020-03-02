4e08d9e6935aaa293ff3707971d98c6066b3a509
Dries Leyers <52953642+DriesLeyers@users.noreply.github.com>
2020-03-02 14:30:02 +0100

    Hotfix

---
+++ SchoolbalDomain/Inschrijving.cs
@@ -0,0 +1,29 @@
+﻿namespace SchoolbalDomain
+{
+    public class Inschrijving
+    {
+        public string naam { get; set; }
+        public string klas { get; set; }
+        public string gast1 { get; set; }
+        public string gast2 { get; set; }
+        public Inschrijving(string naam, string klas)
+        {
+            this.naam = naam;
+            this.klas = klas;
+        }
+        public Inschrijving(string naam, string klas, string gast1)
+        {
+            this.naam = naam;
+            this.klas = klas;
+            this.gast1 = gast1;
+        }
+
+        public Inschrijving(string naam, string klas, string gast1, string gast2)
+        {
+            this.naam = naam;
+            this.klas = klas;
+            this.gast1 = gast1;
+            this.gast2 = gast2;
+        }
+    }
+}
