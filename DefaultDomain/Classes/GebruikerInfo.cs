b3383f358c1d254c1280aceb1d4af1d9ec2c6746
Joren Schelkens <52953553+JorenSchelkens@users.noreply.github.com>
2020-03-06 14:29:22 +0100

    User working

--- DefaultDomain/Classes/GebruikerInfo.cs
+++ DefaultDomain/Classes/GebruikerInfo.cs
@@ -1,20 +1,31 @@
-﻿namespace DefaultDomain.Classes
+﻿using System.Collections.Generic;
+
+namespace DefaultDomain.Classes
 {
     public class GebruikerInfo
     {
         public string Voornaam { get; set; }
         public string Naam { get; set; }
         public string GebruikersNaam { get; set; }
-        public int InternNummer { get; set; }
         public string Status { get; set; }
         public string Geslacht { get; set; }
-        public string Klas { get; set; }
+        public List<Group> Groups { get; set; }
+
+        public bool IsAdmin { get; set; }
+        public bool IsLeerling { get; set; }
 
         //Transactie
 
         public GebruikerInfo()
         {
-            
+            this.Groups = new List<Group>();
+        }
+
+        public void SyncVars()
+        {
+            this.IsLeerling = (this.Groups[0].isKlas) ? true : false;
+
+            // TODO Admin
         }
     }
 }
---
+++ DefaultDomain/Classes/Group.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultDomain.Classes
+{
+    public class Group
+    {
+        public string Name { get; set; }
+        public bool isKlas { get; set; }
+
+        public Group()
+        {
+
+        }
+    }
+}
