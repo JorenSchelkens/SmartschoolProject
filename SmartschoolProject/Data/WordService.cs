2ffc0fd607bfc24cbfc6baa4d719ffc5d83e6b85
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2020-04-22 22:01:03 +0200

    WordDocKlaar

--- SmartschoolProject/Data/WordService.cs
+++ SmartschoolProject/Data/WordService.cs
@@ -26,15 +26,15 @@ namespace SmartschoolProject.Data
             //Create Paragraph styles
             WParagraphStyle style = document.AddParagraphStyle("Normal") as WParagraphStyle;
             style.CharacterFormat.FontName = "Calibri";
-            style.CharacterFormat.FontSize = 11f;
+            style.CharacterFormat.FontSize = 16f;
             style.ParagraphFormat.BeforeSpacing = 0;
-            style.ParagraphFormat.AfterSpacing = 8;
+            style.ParagraphFormat.AfterSpacing = 0;
             style.ParagraphFormat.LineSpacing = 13.8f;
 
             style = document.AddParagraphStyle("Heading 1") as WParagraphStyle;
             style.ApplyBaseStyle("Normal");
             style.CharacterFormat.FontName = "Calibri Light";
-            style.CharacterFormat.FontSize = 16f;
+            style.CharacterFormat.FontSize = 18f;
             style.CharacterFormat.TextColor = Syncfusion.Drawing.Color.FromArgb(46, 116, 181);
             style.ParagraphFormat.BeforeSpacing = 12;
             style.ParagraphFormat.AfterSpacing = 0;
@@ -45,35 +45,75 @@ namespace SmartschoolProject.Data
 
             paragraph.ApplyStyle("Normal");
             paragraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Center;
-            WTextRange textRange = paragraph.AppendText("Inventaris Lokalen") as WTextRange;
-            textRange.CharacterFormat.FontSize = 12f;
+            WTextRange textRange = paragraph.AppendText("Overzicht") as WTextRange;
             textRange.CharacterFormat.FontName = "Calibri";
-            textRange.CharacterFormat.TextColor = Syncfusion.Drawing.Color.Red;
+            textRange.CharacterFormat.TextColor = Syncfusion.Drawing.Color.FromArgb(46, 116, 181);
 
             //Appends paragraph
             paragraph = section.AddParagraph();
             paragraph.ApplyStyle("Heading 1");
             paragraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Center;
             textRange = paragraph.AppendText("Inventaris Lokalen") as WTextRange;
-            textRange.CharacterFormat.FontSize = 18f;
+            textRange.CharacterFormat.FontSize = 20f;
             textRange.CharacterFormat.FontName = "Calibri";
 
-            foreach (Lokaal lokaal in lokalen)
-            {
-                //Appends paragraph
-                paragraph = section.AddParagraph();
-                paragraph.ParagraphFormat.FirstLineIndent = 36;
-                paragraph.BreakCharacterFormat.FontSize = 12f;
-                textRange = paragraph.AppendText("Lokaal " + lokaal.lokaalNr) as WTextRange;
-                textRange.CharacterFormat.FontSize = 20f;
-            }
+            //Eerste cel van tabel
+            IWTable table = section.AddTable();
+            table.ResetCells(lokalen.Count(), 3);
+            WTableCell firstCell = table.Rows[0].Cells[0];
+            firstCell.Width = 100;
+            paragraph = firstCell.AddParagraph();
+            paragraph.ApplyStyle("Normal");
+            textRange = paragraph.AppendText("Lokaal") as WTextRange;
+            textRange.CharacterFormat.Bold = true;
 
-            //Appends paragraph
-            paragraph = section.AddParagraph();
-            paragraph.ParagraphFormat.FirstLineIndent = 36;
-            paragraph.BreakCharacterFormat.FontSize = 12f;
-            textRange = paragraph.AppendText("Hello Earth") as WTextRange;
-            textRange.CharacterFormat.FontSize = 12f;
+            //Tweede cel van tabel
+            WTableCell secondCell = table.Rows[0].Cells[1];
+            secondCell.Width = 180;
+            paragraph = secondCell.AddParagraph();
+            paragraph.ApplyStyle("Normal");
+            textRange = paragraph.AppendText("Details") as WTextRange;
+            textRange.CharacterFormat.Bold = true;
+
+            //Derde cel van tabel
+            WTableCell thirdCell = table.Rows[0].Cells[2];
+            thirdCell.Width = 156;
+            paragraph = thirdCell.AddParagraph();
+            paragraph.ApplyStyle("Normal");
+            textRange = paragraph.AppendText("Voorwerpen") as WTextRange;
+            textRange.CharacterFormat.Bold = true;
+
+            for (int i = 1; i < lokalen.Count(); i++) 
+            {
+
+                //Lokaal
+                firstCell = table.Rows[i].Cells[0];
+                firstCell.Width = 100;
+                paragraph = firstCell.AddParagraph();
+                textRange = paragraph.AppendText("Lokaal " + lokalen[i].lokaalNr) as WTextRange;
+                textRange.CharacterFormat.FontSize = 14f;
+
+                //Details per lokaal
+                secondCell = table.Rows[i].Cells[1];
+                secondCell.Width = 180;
+                paragraph = secondCell.AddParagraph();
+                textRange = paragraph.AppendText("Nummer : " + lokalen[i].lokaalNr + "\n" +
+                                                 "Verantwoordelijke : " + lokalen[i].lokaalVerantwoordelijke + "\n" +
+                                                 "Actief/Inactief : " + (lokalen[i].isActief ? "Actief" : "Inactief")) as WTextRange;
+                textRange.CharacterFormat.FontSize = 13f;
+
+                //Voorwerpen per lokaal
+                for (int j = 1; j < lokalen[i].Voorwerpen.Count(); j++)
+                {
+                    thirdCell = table.Rows[j].Cells[2];
+                    thirdCell.Width = 170;
+                    paragraph = thirdCell.AddParagraph();
+                    textRange = paragraph.AppendText("- " + lokalen[i].Voorwerpen[j].aantal + " x " + lokalen[i].Voorwerpen[j].voorwerpNaam + "\n" +
+                                                            (lokalen[i].Voorwerpen[j].defect ? "defect" : "niet defect")) as WTextRange;
+                    textRange.CharacterFormat.FontSize = 13f;
+                }
+
+            }
 
             //Saves the Word document to MemoryStream
             MemoryStream stream = new MemoryStream();
